b95430184418f3d877cbdf139ecdaad63815d606
shadowmoonezios <agushifelde@hotmail.com>
2025-09-25 17:41:18 +0300

    Add error handling middleware for improved error management

--- server.js
+++ server.js
@@ -13,6 +13,12 @@ mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopol
   .then(() => console.log('Connected to MongoDB...'))
   .catch(err => console.error('Could not connect to MongoDB:', err));
 
+// Error handling middleware
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
+});
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
