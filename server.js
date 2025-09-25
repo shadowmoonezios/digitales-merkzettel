2224413bfd0734afaabad696fe50fae8c9c887d9
shadowmoonezios <agushifelde@hotmail.com>
2025-09-25 17:39:32 +0300

    Update logging message to English for consistency.

--- server.js
+++ server.js
@@ -15,5 +15,5 @@ mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopol
   .catch(err => console.log(err));
 
 app.listen(PORT, () => {
-  console.log(`Server läuft auf Port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
