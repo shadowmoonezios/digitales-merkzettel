709f82a7c23c1073b23d2488dadf1bfed0254566
shadowmoonezios <agushifelde@hotmail.com>
2025-09-25 17:44:10 +0300

    Improve error handling middleware to send error message to client.

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopol
 // Error handling middleware
 app.use((err, req, res, next) => {
   console.error(err.stack);
-  res.status(500).send('Something broke!');
+  res.status(500).send({ error: 'Something broke!', details: err.message });
 });
 
 app.listen(PORT, () => {
