2fac89266cb771a705dc111804f73246232a818b
shadowmoonezios <agushifelde@hotmail.com>
2025-09-25 17:39:05 +0300

    Update MongoDB connection logging message for clarity.

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 5000;
 app.use(bodyParser.json());
 
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log('MongoDB verbunden...'))
+  .then(() => console.log('Connected to MongoDB...'))
   .catch(err => console.log(err));
 
 app.listen(PORT, () => {
